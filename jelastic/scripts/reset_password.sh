8597b800c233da964cc8beb681eda65df0e23d19
Dimka Tsurko <dt@jelastic.com>
2017-08-02 14:47:22 +0300

    JE-33629 No AUTH on redis

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -7,6 +7,7 @@ function _setPassword() {
         service cartridge stop > /dev/null 2>&1;
         echo $J_OPENSHIFT_APP_ADM_PASSWORD > ${CARTRIDGE_HOME}/env/REDIS_PASSWORD;
         export REDIS_PASSWORD=$J_OPENSHIFT_APP_ADM_PASSWORD;
+        sed -i -re "/requirepass/s/^\s*requirepass.*/requirepass ${J_OPENSHIFT_APP_ADM_PASSWORD}/" ${CARTRIDGE_HOME}/conf/redis.conf
         saveEnvironmentVariables;
         service cartridge start > /dev/null 2>&1;
 	chmod 555 /opt/repo/env
